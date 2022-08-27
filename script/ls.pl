bcfec61bf8f4e571e26cd16fda37bf59ae300da7
Max Maischein <corion@corion.net>
2022-08-27 08:25:38 +0200

    Use ->entries()

--- script/ls.pl
+++ script/ls.pl
@@ -1,3 +1,25 @@
 #!perl
+use 5.020;
+use Filesys::DB;
 use DBIx::RunSQL;
-DBIx::RunSQL->handle_command_line('filesys-db', \@ARGV);
+use Getopt::Long;
+
+GetOptions(
+    'mountpoint|m=s' => \my $mountpoint,
+    'alias|a=s' => \my $mount_alias,
+);
+
+my $sql = join " ", @ARGV;
+
+$mount_alias //= '${MOUNT}';
+$mountpoint //= $ARGV[0];
+
+
+my $store =Filesys::DB->new(
+    mountpoints => {
+        $mount_alias => $mountpoint,
+    },
+);
+print DBIx::RunSQL->format_results(
+    sth => $store->entries( $sql ),
+);
