daaf49136281e748a141edf1ab145cba8080c17f
Max Maischein <corion@corion.net>
2022-08-28 21:46:49 +0200

    Make `ls.pl` more usable

--- script/ls.pl
+++ script/ls.pl
@@ -3,23 +3,50 @@ use 5.020;
 use Filesys::DB;
 use DBIx::RunSQL;
 use Getopt::Long;
+use YAML 'LoadFile';
+use PerlX::Maybe;
 
 GetOptions(
     'mountpoint|m=s' => \my $mountpoint,
     'alias|a=s' => \my $mount_alias,
+    'config|f=s' => \my $config_file,
+    'columns|c=s' => \my @columns,
 );
 
+@columns = 'filename' unless @columns;
 my $sql = join " ", @ARGV;
 
-$mount_alias //= '${MOUNT}';
-$mountpoint //= $ARGV[0];
+my $config = {};
+my $user_config = {};
+if(! defined $config_file ) {
+    if ( 'filesys-db.yaml' ) {
+        $config_file = 'filesys-db.yaml';
+    } else {
+        $user_config = {
+            mountpoints => [
+                {
+                  alias => $mount_alias // '${MOUNT}',
+                  directory => $mountpoint //  $ARGV[0],
+                }
+            ],
+        }
+    }
+}
+if( $config_file ) {
+    $user_config = LoadFile( $config_file );
+};
+$user_config->{mountpoints} //= {};
+$config->{mountpoints} = $user_config->{mountpoints};
 
+@columns = map { split /\,/ } @columns;
 
-my $store =Filesys::DB->new(
+my $store = Filesys::DB->new(
     mountpoints => {
-        $mount_alias => $mountpoint,
+        %{ $config->{mountpoints} },
+        maybe $mount_alias => $mountpoint,
     },
 );
+
 print DBIx::RunSQL->format_results(
-    sth => $store->entries( $sql ),
+    sth => $store->entries( \@columns, $sql ),
 );
