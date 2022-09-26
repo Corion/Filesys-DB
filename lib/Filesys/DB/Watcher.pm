65389195a2961ac3fb4797080c303d031c4a1b78
Max Maischein <corion@cpan.org>
2022-09-26 13:04:19 +0200

    Support both kinds of changes
    Maybe the Win32 thing could look more like ChangeNotify instead of ChangeNotify::Simple?

--- lib/Filesys/DB/Watcher.pm
+++ lib/Filesys/DB/Watcher.pm
@@ -21,7 +21,7 @@ Blocks and watches all mountpoints for updates
 
 =cut
 
-sub watch( $self, %options ) {
+sub watch_win32( $self, %options ) {
     require Filesys::Notify::Win32::ReadDirectoryChanges;
     my $w = Filesys::Notify::Win32::ReadDirectoryChanges->new(subtree => 1);
     for my $mp (@{ $self->mountpoints }) {
@@ -33,5 +33,25 @@ sub watch( $self, %options ) {
         $cb->($ev);
     }
 }
+
+sub watch_changenotify( $self, %options ) {
+    require Filesys::DB::Watcher;
+    my $w = Filesys::DB::Watcher->new(subtree => 1);
+    for my $mp (@{ $self->mountpoints }) {
+        $w->watch_directory( path => $self->store->to_local($mp,''));
+    };
+    my $q = $w->queue;
+    my $cb = $options{ cb };
+    while( my $ev = $q->dequeue) {
+        $cb->($ev);
+    }
+}
+
+sub watch( $self, %options ) {
+    goto &watch_win32 if($^O eq 'MSWin32');
+    goto &watch_changenotify;
+}
+
+1;
 
 1;
